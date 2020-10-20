02227276df5299933293016abbb3d65b07d599f7
frmauro <frmauro8@gmail.com>
2020-10-20 18:29:11 -0300

    Implementation dashboard component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from "rxjs";
 
 import { Product } from "./models/Product";
@@ -11,18 +12,27 @@ import { MessageService } from "./message.service";
 })
 export class ProductService {
 
-  constructor(private messageService: MessageService) { }
+  private productsUrl = 'api/products';  // URL to web api
+
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
 
   getProducts(): Observable<Product[]> {
     // TODO: send the message _after_ fetching the products
-    this.messageService.add('ProductService: fetched products');
+    //this.messageService.add('ProductService: fetched products');
     return of(PRODUCTS);
   }
 
   getProduct(id: Number): Observable<Product>{
     // TODO: send the message _after_ fetching the product id
-    this.messageService.add(`ProductService: fetched product id=${id}`);
+    //this.messageService.add(`ProductService: fetched product id=${id}`);
      return of(PRODUCTS.find(product => product.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 
