8ab643afbdd6d6c6523012956e98e42e4b5be336
frmauro <frmauro8@gmail.com>
2020-10-21 14:41:09 -0300

    Implementation getProduct function

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from "rxjs";
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from "./models/Product";
 import { PRODUCTS } from "./models/mockProduct";
@@ -12,7 +13,7 @@ import { MessageService } from "./message.service";
 })
 export class ProductService {
 
-  private productsUrl = 'api/products';  // URL to web api
+  private productsUrl = 'http://localhost:8087/Product';  // URL to web api
 
   constructor(
     private http: HttpClient, 
@@ -21,19 +22,49 @@ export class ProductService {
   getProducts(): Observable<Product[]> {
     // TODO: send the message _after_ fetching the products
     //this.messageService.add('ProductService: fetched products');
-    return of(PRODUCTS);
+    //return of(PRODUCTS);
+    return this.http.get<Product[]>(this.productsUrl)
+    .pipe(
+      tap(_ => this.log('fetched products')),
+      catchError(this.handleError<Product[]>('getProducts', []))
+    );
   }
 
   getProduct(id: Number): Observable<Product>{
     // TODO: send the message _after_ fetching the product id
     //this.messageService.add(`ProductService: fetched product id=${id}`);
-     return of(PRODUCTS.find(product => product.id === id));
+     //return of(PRODUCTS.find(product => product.id === id));
+     const url = `${this.productsUrl}/${id}`;
+     return this.http.get<Product>(url).pipe(
+      tap(_ => this.log(`fetched product id=${id}`)),
+      catchError(this.handleError<Product>(`getProduct id=${id}`))
+     );
   }
 
-  /** Log a HeroService message with the MessageService */
+  /** Log a ProductService message with the MessageService */
   private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
+    this.messageService.add(`productService: ${message}`);
   }
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
 
 
 }
